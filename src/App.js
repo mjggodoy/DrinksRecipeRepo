c1245b314064ddaba6556f45b5eb67517f2230c2
mjggodoy <mjggodoy@gmail.com>
2020-04-30 20:36:59 +0200

    Setting the app. Inlcude a header component and a form.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import Header from './components/Header';
+import Form from './components/form';
+
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+        <div className="container mt-5">
+          <div className="row">
+            <Form/>
+          </div>
+        </div>
+    </Fragment>
+  );
+}
+
+export default App;
