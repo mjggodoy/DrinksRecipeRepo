fbd7c0625460262fc7a1308de5501e27a30b9fd5
mjggodoy <mjggodoy@gmail.com>
2020-05-03 21:06:39 +0200

    Include axios to retrieve information from the API through the asyn getAllCategoriesFromApi() function

--- src/context/CategoryContext.js
+++ src/context/CategoryContext.js
@@ -1,10 +1,33 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useState, useEffect} from 'react';
+import axios from 'axios';
 
 export const CategoryContext = createContext();
 
 const CategoryProvider = (props) => {
     
-    const [category, saveCategory] = useState("");
+    const [category, saveCategory] = useState([]);
+
+    useEffect(() => {
+        const getAllCategoriesFromApi = async() => {
+            const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list";
+            const response = await axios.get(url).then((response) => {
+                const categoryDrinks = response.data.drinks;
+                saveCategory(categoryDrinks);
+            }).catch((error) => {
+                if (error.response) {
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    console.log('error, request', error.request);
+                } else {
+                    console.log('Error', error.message);
+                }
+                    console.error(error.config);
+            });
+        }
+        getAllCategoriesFromApi();
+    }, [])
 
     return(
         <CategoryContext.Provider
