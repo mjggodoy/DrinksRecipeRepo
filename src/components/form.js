f009b55fa3f7d7d8d5514f95d5c2fa9231dad328
mjggodoy <mjggodoy@gmail.com>
2020-05-03 20:00:58 +0200

    Create a form in Form component.

--- src/components/form.js
+++ src/components/form.js
@@ -2,7 +2,35 @@ import React from 'react';
 
 const Form = () => {
     return (
-        <h1>Form goes here</h1>
+        <form className="col-12">
+            <fieldset className="text-center"><legend>Search drinks per category/ingredient</legend></fieldset>
+                <div className="row mt-5">
+                    <div className="col-md-4">
+                    <input
+                        name="ingredientName"
+                        className="form-control"
+                        type="text"
+                        placeholder="Introduce an ingredient"
+                        
+                    />
+                </div>
+                <div className="col-md-4">
+                    <select
+                        className="form-control"
+                        name = "category"
+                    >
+                        <option value="">--Select a category--</option>
+                    </select>
+                </div>
+                <div className="col-md-4">
+                    <input
+                        type="submit"
+                        className="btn btn-block btn-primary"
+                        value="Search recipes"
+                    />
+                </div>
+            </div>
+        </form>
     );
 }
 
