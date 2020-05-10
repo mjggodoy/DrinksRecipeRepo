90358123cf7df826bd25d5fef890fc887108f314
mjggodoy <mjggodoy@gmail.com>
2020-05-10 21:57:12 +0200

    Retrive information from API for that drink.
    Y

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -5,7 +5,34 @@ export const ModalContext = createContext();
 
 const ModalProvider = (props) => {
 
-    const [idrecipe, saveIdRecipe] = useState(null);
+    const [idrecipe, saveIdRecipe] = useState();
+    const [recipe, saveRecipe] = useState({});
+
+
+    useEffect(() => {
+        const retrieveRecipeById = async() => {
+            if (idrecipe !== undefined) {
+                const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idrecipe}`;
+                await axios.get(url).then((response) => {
+                    const recipes = response.data.drinks[0];
+                    saveRecipe(recipes);
+                }).catch((error) => {
+                    if (error.response) {
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        console.log(error.response.headers);
+                    } else if (error.request) {
+                        console.log('error, request', error.request);
+                    } else {
+                        console.log('Error', error.message);
+                    }
+                        console.error(error.config);
+                    });
+            }
+            
+        }   
+        retrieveRecipeById();
+    }, [idrecipe]); 
 
     return(
         <ModalContext.Provider
